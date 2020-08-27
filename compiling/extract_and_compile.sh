4549c47e0d76158c813a85bbde15d8a5ea0056ea
Ben <burge337@morris.umn.edu>
2020-08-27 00:04:20 -0500

    Get file extraction working into proper directory.
    Also changed around some of the comments a bit.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 ##Copying the lab requirements above the lines of code that perform the given task may remove later.
 
@@ -7,7 +7,11 @@
 #The number taken in and used for our C program
 arg_number=$1
 
-##Extracts the contents of the tar archive NthPrime.tgz into the current directory
+#Make a sub-directory to store extracted files
+mkdir NthPrime
+
+##Extracts the contents of the tar archive NthPrime.tgz into the NthPrime directory
+tar -zxf NthPrime.tgz --one-top-level
 
 #Extracting the tar file contents into our desired directory
 
