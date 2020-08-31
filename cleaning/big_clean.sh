4f0057f8d5c0e463d6ce67c06f56c83cfb42c934
Ben <burge337@morris.umn.edu>
2020-08-30 21:55:10 -0500

    The latest (failed) attempts at getting the cleaned tar file to create.
    Largely running into cowardly empty directory errors that I don't understand.
    I think I have the code for the actual deletion of marked files up and running, but I can't actually tell
    until I can actually tar the files after the fact for the tests.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,8 +3,32 @@
 #Capturing the tar file we will be cleaning
 arg_tar=$1
 
+#Capturing starting directory
+cleaning=$(pwd)
+
 #Make a temporary for our work
 SCRATCH=`mktemp --directory`
 
 #Extract the given tar files into the temporary directory
-tar zxf $arg_tar --directory $SCRATCH
+tar -zxf $arg_tar --directory $SCRATCH
+
+cd $SCRATCH || exit
+
+#remove all files with the string "DELETE ME!" in them
+#solution from https://superuser.com/questions/585391/on-linux-how-can-i-find-all-files-that-contain-a-string-and-delete-them/585436
+grep -lrIZ 'DELETE ME!' . | xargs -0 rm -f --
+
+#Grab the basename of the file in preperation of making our new, clean tar file
+base_tar=`basename $arg_tar`
+
+#Swap to the Scratch directory to make the new tar file
+#cd $SCRATCH || exit
+
+#Create file name for newly cleaned tar file (includes .tgz on end)
+clean_name="cleaned_"
+clean_name+=$base_tar
+
+cd $base_tar
+#Create a compressed archive of all the files that weren't deleted. Should be named based on the original tar file name
+#but prefix with cleaned_
+tar -czf $clean_name -C $cleaning .
